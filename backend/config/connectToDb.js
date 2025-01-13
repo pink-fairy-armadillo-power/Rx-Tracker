3fe4f50f446f0b3387aec3a0c05a05e6c341f44e
Peter Limburg <129231266+Peter-Limburg@users.noreply.github.com>
2025-01-13 12:23:21 -0500

    Co-authored-by: Moe <wmoew@users.noreply.github.com> Co-authored-by: Oliver Antonio Fajardo <oliverafajardoucb@gmail.com>

--- backend/config/connectToDb.js
+++ backend/config/connectToDb.js
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 // If you're not in production, load the environment variables
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
 async function connectToDb() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Successfully connected to the database");
+    console.log('Successfully connected to the database');
   } catch (error) {
-    console.error("Error connecting to the database:", error);
+    console.error('Error connecting to the database:', error);
     throw error; // Ensure the error is thrown so the server doesn't start
   }
 }
